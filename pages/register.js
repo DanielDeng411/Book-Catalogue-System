f0bec3826831fdfe572f48e18eff1b2ec1e1fb86
Evelyn-Yuling <evelynleeeeeee@gmail.com>
2024-11-27 23:35:39 -0500

    Update register.js

--- pages/register.js
+++ pages/register.js
@@ -1,1 +1,17 @@
-
+function validateForm() {
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirmPassword').value;
+    const errorElement = document.getElementById('passwordError');
+    if (password.length < 6) {
+        errorElement.textContent = 'Password length must be at least 6 characters!';
+        return false;
+    }
+    if (password !== confirmPassword) {
+        errorElement.textContent = 'Passwords do not match!';
+        return false;
+    }
+    
+    errorElement.textContent = '';
+    document.getElementById('registerForm').submit();
+    return true;
+}
