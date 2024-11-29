3e6a5adc8c80d726f3c464ee2f1e4ab349f86108
DanielDeng411 <DanielDeng1522@gmail.com>
2024-11-29 13:00:45 -0500

    rating return

--- database/review.sql
+++ database/review.sql
@@ -1,8 +1,8 @@
-CREATE TABLE review (
+CREATE TABLE ratings (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
     book_id INT NOT NULL,
-    review VARCHAR(510),
+    rating INT CHECK (rating BETWEEN 1 AND 5),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
     FOREIGN KEY (book_id) REFERENCES books(id) ON DELETE CASCADE
